b66cf3ffbdb09861fde84a80706376966ad5d953
qpakr38 <zczc0819@gmail.com>
2022-04-03 16:25:14 +0900

    REFACTORY : Interface 로변경하여 확장이 가능하도록 변경함.

--- src/main/java/study/tobyspring/dao/SimpleConnectionMaker.java
+++ src/main/java/study/tobyspring/dao/SimpleConnectionMaker.java
@@ -4,11 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
-    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mariadb://qpakr38.iptime.org:3306/tobySpring", "tobySpring", "tobySpring");
-
-        return connection;
-    }
+public interface SimpleConnectionMaker {
+    Connection makeNewConnection() throws ClassNotFoundException, SQLException;
 }
